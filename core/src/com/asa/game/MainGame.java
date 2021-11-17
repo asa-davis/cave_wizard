5b08cd574a18f9683220eb0d529c0033e7a095cd
Asa Davis <asadavis1002@gmail.com>
2021-11-17 00:43:08 -0700

    draw texture inside polygon - for later

--- core/src/com/asa/game/MainGame.java
+++ core/src/com/asa/game/MainGame.java
@@ -5,8 +5,14 @@ import com.asa.game.Map.MapRenderer;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Polygon;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.awt.*;
 
 public class MainGame extends ApplicationAdapter {
 	ShapeRenderer shapeRenderer;
@@ -16,6 +22,11 @@ public class MainGame extends ApplicationAdapter {
 	MapRenderer mapRenderer;
 	Wizard wizard;
 
+	//for later - use one big floor texture to draw only the visible polygon
+	float[] polyVert = new float[] {0, 0, 200, 0, 100, 300, 0, 0};
+	TextureRegion floor;
+	PolygonSpriteBatch polySpriteBatch;
+
 	@Override
 	public void create () {
 		shapeRenderer = new ShapeRenderer();
@@ -24,6 +35,10 @@ public class MainGame extends ApplicationAdapter {
 		map = new Map(32, 16);
 		mapRenderer = new MapRenderer(map, shapeRenderer, lineRenderer);
 		wizard = new Wizard(map.getCenter());
+
+		//for later - use one big floor texture to draw only the visible polygon
+		polySpriteBatch = new PolygonSpriteBatch();
+		floor = new TextureRegion(new Texture("raw_textures/floor.jpg"));
 	}
 
 	@Override
@@ -42,15 +57,20 @@ public class MainGame extends ApplicationAdapter {
 		//mapRenderer.drawRays();
 		lineRenderer.end();
 
+		//for later - use one big floor texture to draw only the visible polygon
+		drawFloor();
+
 		handleInput();
 	}
 
 	private void handleInput() {
 		if(Gdx.input.isKeyPressed(Input.Keys.W)) {
 			wizard.setPosition(wizard.getPosition().add(0, 1));
+			polyVert[4] += 5;
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.S)) {
 			wizard.setPosition(wizard.getPosition().add(0, -1));
+			polyVert[4] -= 5;
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.D)) {
 			wizard.setPosition(wizard.getPosition().add(1, 0));
@@ -59,10 +79,19 @@ public class MainGame extends ApplicationAdapter {
 			wizard.setPosition(wizard.getPosition().add(-1, 0));
 		}
 	}
+
+	//for later - use one big floor texture to draw only the visible polygon
+	private void drawFloor() {
+		PolygonRegion floorTexturePolygon = new PolygonRegion(floor, polyVert, new short[] {0, 1, 2, 0, 3, 2});
+		polySpriteBatch.begin();
+		polySpriteBatch.draw(floorTexturePolygon, 800, 200);
+		polySpriteBatch.end();
+	}
 	
 	@Override
 	public void dispose () {
 		shapeRenderer.dispose();
 		lineRenderer.dispose();
+		polySpriteBatch.dispose();
 	}
 }
